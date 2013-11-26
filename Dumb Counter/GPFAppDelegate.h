58589d40f7c9c3c6fcf63c5c4be8314c4cd6ab04
Greg Fiumara <gregory.fiumara@nist.gov>
2013-11-26 08:33:57 -0500

    Initial Commit

---
+++ Dumb Counter/GPFAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  GPFAppDelegate.h
+//  Dumb Counter
+//
+//  Created by Greg Fiumara on 11/26/13.
+//  Copyright (c) 2013 Greg Fiumara. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GPFAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Dumb Counter/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Dumb Counter
+//
+//  Created by Greg Fiumara on 11/26/13.
+//  Copyright (c) 2013 Greg Fiumara. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "GPFAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([GPFAppDelegate class]));
+	}
+}
