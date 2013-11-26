6cf250b2dfa16154d2b64fda6a085cdc5a8c3c1c
Greg Fiumara <gregory.fiumara@nist.gov>
2013-11-26 15:15:18 -0500

    Add two invisible views to make layout stretch to look correct in landscape and on 3.5" devices.

--- Dumb Counter/GPFDumbCounterViewController.h
+++ Dumb Counter/GPFDumbCounterViewController.h
@@ -12,6 +12,9 @@
 
 @property (weak, nonatomic) IBOutlet UILabel *incrementLabel;
 @property (weak, nonatomic) IBOutlet UILabel *totalLabel;
+@property (weak, nonatomic) IBOutlet UIButton *incrementButton;
+@property (weak, nonatomic) IBOutlet UIButton *decrementButton;
+@property (weak, nonatomic) IBOutlet UIButton *addButton;
 
 - (IBAction)decrementButtonPressed:(UIButton *)sender;
 - (IBAction)incrementButtonPressed:(UIButton *)sender;
